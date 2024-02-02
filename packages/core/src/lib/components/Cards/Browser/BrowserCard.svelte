67eb9f110f70ea0a18b1ad04e69ad934f4592cb8
Felix Tesche <felix.tesche@gmail.com>
2024-02-02 14:35:40 +0100

    url bar switches between root domain and full domain onEdit

--- packages/core/src/lib/components/Cards/Browser/BrowserCard.svelte
+++ packages/core/src/lib/components/Cards/Browser/BrowserCard.svelte
@@ -90,6 +90,38 @@
   $: if (!editing && $url !== 'about:blank') {
     value = $url ?? ''
   }
+
+  $: if (!editing) {
+    value = generateRootDomain(value)
+  }
+
+  $: if(editing) {
+    value = $url ?? ''
+  }
+
+
+  function generateRootDomain (url: string | URL) : string {
+      if(url === ''){ return }
+      if (typeof url === 'string')
+          url = new URL(url);
+
+      const domain = url.hostname;
+      const elems = domain.split('.');
+      const iMax = elems.length - 1;
+
+      const elem1 = elems[iMax - 1];
+      const elem2 = elems[iMax];
+
+      const isSecondLevelDomain = iMax >= 3 && (elem1 + elem2).length <= 5;
+      return (isSecondLevelDomain ? elems[iMax - 2] + '.' : '') + elem1 + '.' + elem2;
+  }
+
+  const handleFinishLoading = () => {
+    log.debug('finished loading', get(card))
+    dispatch('load', get(card))
+  }
+
+  
 </script>
 
 <div class="browser-card">
