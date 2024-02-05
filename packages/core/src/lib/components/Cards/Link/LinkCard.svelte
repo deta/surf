39f168b8a4c230283c07cf98f334b03cf73979b5
Maximilian Schiller <hello@mxis.ch>
2024-02-05 13:51:36 +0100

    fix: bookmark link causing broken app and title parsing (#30)

--- packages/core/src/lib/components/Cards/Link/LinkCard.svelte
+++ packages/core/src/lib/components/Cards/Link/LinkCard.svelte
@@ -16,7 +16,7 @@
     try {
       const url = new URL($card.data.url)
 
-      const hostname = url.hostname.split('.')[0]
+      const hostname = url.hostname.split('.').slice(-2, -1).join('')
       title = hostname[0].toUpperCase() + hostname.slice(1)
       subtitle = `${url.hostname}${url.pathname}`
     } catch (e) {
@@ -25,14 +25,10 @@
   })
 </script>
 
-<a href={$card.data.url} class="link-card">
+<a href={$card.data.url} target="_blank" class="link-card">
   <div class="details">
     <div class="title">{title}</div>
     <div class="subtitle">{subtitle}</div>
-  </div>
-
-  <div class="actions">
-    <button>↗</button>
   </div>
 </a>
 
@@ -64,23 +60,5 @@
   .subtitle {
     font-size: 1rem;
     font-weight: 300;
-  }
-
-  .actions {
-    flex-shrink: 0;
-    width: min-content;
-
-    button {
-      width: 3rem;
-      height: 3rem;
-      border-radius: 50%;
-      border: none;
-      outline: none;
-      background: transparent;
-      color: inherit;
-      font-size: 1rem;
-      font-weight: 500;
-      cursor: pointer;
-    }
   }
 </style>
