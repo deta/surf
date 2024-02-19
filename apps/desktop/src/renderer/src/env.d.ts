74199682d1b1868c768292296ec927bc330569c7
Felix Tesche <felix.tesche@gmail.com>
2024-02-19 16:44:59 +0100

    added demo url via env (#152)

--- apps/desktop/src/renderer/src/env.d.ts
+++ apps/desktop/src/renderer/src/env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly R_VITE_BUILD_TAG?: string
   readonly R_VITE_APP_VERSION?: string
   readonly R_VITE_TELEMETRY_API_KEY?: string
+  readonly R_VITE_CHEATSHEET_URL?: string
 }
 
 interface ImportMeta {
--- packages/core/src/lib/utils/demoHorizon.ts
+++ packages/core/src/lib/utils/demoHorizon.ts
@@ -40,72 +40,16 @@ export const initDemoHorizon = async (horizon: Horizon) => {
     }
   })
 
-  // horizon.addCard({
-  //     ...{x: 50, y: 400, width: 400, height: 400},
-  //     type: 'text',
-  //     data: {
-  //         content: {"type":"doc","content":[{"type":"heading","attrs":{"level":3},"content":[{"type":"text","text":"How to use your Horizon"}]},{"type":"paragraph","content":[{"type":"text","text":"With this version you can essentially do browse the web for the first time! Here's a quick guide to get you started."}]},{"type":"paragraph"},{"type":"bulletList","content":[{"type":"listItem","content":[{"type":"paragraph","content":[{"type":"text","marks":[{"type":"bold"}],"text":"Note-Taking"},{"type":"text","text":" Need to jot down thoughts? Simply draw on the screen to create a note."}]}]},{"type":"listItem","content":[{"type":"paragraph","content":[{"type":"text","marks":[{"type":"bold"}],"text":"New Browser Window"},{"type":"text","text":" Simply hold "},{"type":"text","marks":[{"type":"code"}],"text":"CMD (macOS)"},{"type":"text","text":" "},{"type":"text","marks":[{"type":"code"}],"text":"Ctrl (Windows)"},{"type":"text","text":" while drawing. This will create a new spatial browser."}]}]}]}]}
-  //     }
-  // })
-
-  horizon.addCardBrowser('https://en.wikipedia.org/wiki/Common_sunflower', {
-    x: 550,
-    y: 50,
-    width: 600,
-    height: 800
-  })
-
-  // horizon.addCard({
-  //     ...{x: 1200, y: 50, width: 300, height: 220},
-  //     type: 'text',
-  //     data: {
-  //         content: {"type":"doc","content":[{"type":"heading","attrs":{"level":3},"content":[{"type":"text","text":"Demo Zone: Drag stuff from the web."}]},{"type":"paragraph","content":[{"type":"text","text":"Ever wanted to just save an image or a text for later? Try it out by dragging images or text onto your horizon directly."}]},{"type":"paragraph"}]}
-  //     }
-  // })
-
-  // Set Card for drag and drop demo
-  // try {
-  //     const response = await fetch(imageDND);
-  //     const imageBlob = await response.blob();
-
-  //     await horizon.addCardFile(imageBlob, {
-  //         x: 1200,
-  //         y: 280,
-  //         width: 400,
-  //         height: 250
-  //     });
-  // } catch (error) {
-  //     console.error('Error in initDemoHorizon:', error);
-  // }
-
-  // horizon.addCard({
-  //     ...{x: 1800, y: 50, width: 300, height: 220},
-  //     type: 'text',
-  //     data: {
-  //         content: {"type":"doc","content":[{"type":"heading","attrs":{"level":3},"content":[{"type":"text","text":"Lets see your Horizons"}]},{"type":"paragraph","content":[{"type":"text","text":"By pressing "},{"type":"text","marks":[{"type":"code"}],"text":"CMD"},{"type":"text","text":" / "},{"type":"text","marks":[{"type":"code"}],"text":"Ctrl"},{"type":"text","text":" + "},{"type":"text","marks":[{"type":"code"}],"text":"O"},{"type":"text","text":" or pinching with your trackpad, you get into the horizon overview, from where you can select, create and delete your horizons."}]}]}
-  //     }
-  // })
-
-  // // Set Card for overview
-  // try {
-  //     const response = await fetch(imageOverview);
-  //     const imageBlob = await response.blob();
-
-  //     await horizon.addCardFile(imageBlob, {
-  //         x: 1800,
-  //         y: 300,
-  //         width: 600,
-  //         height: 500
-  //     });
-  // } catch (error) {
-  //     console.error('Error in initDemoHorizon:', error);
-  // }
-
-  // horizon.addCard({
-  //     ...{x: 2700, y: 300, width: 300, height: 400},
-  //     type: 'text',
-  //     data: {
-  //         content: {"type":"doc","content":[{"type":"heading","attrs":{"level":3},"content":[{"type":"text","text":"Want to have another Horizon?"}]},{"type":"paragraph","content":[{"type":"text","text":"Simply create a new Horizon by"}]},{"type":"bulletList","content":[{"type":"listItem","content":[{"type":"paragraph","content":[{"type":"text","text":"Pressing "},{"type":"text","marks":[{"type":"code"}],"text":"CMD"},{"type":"text","text":" / "},{"type":"text","marks":[{"type":"code"}],"text":"Ctrl"},{"type":"text","text":" + "},{"type":"text","marks":[{"type":"code"}],"text":"N"}]}]}]},{"type":"paragraph"},{"type":"paragraph","content":[{"type":"text","text":"You also can create a new Horizon with a new Browser by:"}]},{"type":"bulletList","content":[{"type":"listItem","content":[{"type":"paragraph","content":[{"type":"text","text":"Pressing "},{"type":"text","marks":[{"type":"code"}],"text":"CMD"},{"type":"text","text":" / "},{"type":"text","marks":[{"type":"code"}],"text":"Ctrl"},{"type":"text","text":" + "},{"type":"text","marks":[{"type":"code"}],"text":"T"}]}]}]}]}
-  //     }
-  // })
+  // Use environment variable for the cheatsheet URL
+  const cheatsheetUrl = import.meta.env.R_VITE_CHEATSHEET_URL
+  if (typeof cheatsheetUrl === 'string' && cheatsheetUrl.length > 0) {
+    horizon.addCardBrowser(cheatsheetUrl, {
+      x: 550,
+      y: 50,
+      width: 600,
+      height: 800
+    })
+  } else {
+    console.error('R_VITE_CHEATSHEET_URL is not defined or invalid.')
+  }
 }
