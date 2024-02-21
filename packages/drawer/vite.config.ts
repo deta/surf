31f6fcf9adec0bde5659b45152608c57416f30b3
Maximilian Schiller <hello@mxis.ch>
2024-02-21 15:00:14 +0100

    add drawer package

---
+++ packages/drawer/vite.config.ts
@@ -0,0 +1,29 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/lib/main.ts'),
+      name: 'Index',
+      // the proper extensions will be added
+      fileName: 'index'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['svelte'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          svelte: 'Svelte'
+        }
+      }
+    }
+  }
+})
