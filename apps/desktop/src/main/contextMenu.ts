871dbc11f46566efd9628e9dd4a6c3ee736605e5
Maximilian Schiller <hello@mxis.ch>
2024-02-23 18:56:40 +0100

    feat: add search and create card context menu items to browser card (#161)
    * feat: set app as default http protocol client
    
    * wip: handle url
    
    * fix: build on linux
    
    * feat: handle url open
    
    * fix
    
    * focus window
    
    * fix: mc
    
    * feat: set app as default browser from app menu
    
    * feat: add search and create card  context menu items to browser card
    
    * menu fix
    
    * fix: open-url race condition
    
    * fix: wait for app to finish starting before passing opened URL to it
    
    * cleanup
    
    * add comment

--- apps/desktop/src/main/contextMenu.ts
+++ apps/desktop/src/main/contextMenu.ts
@@ -1,8 +1,42 @@
 import contextMenu from 'electron-context-menu'
+import { getMainWindow } from './mainWindow'
 
 export function setupContextMenu(options: contextMenu.Options = {}) {
   const defaultOpts: contextMenu.Options = {
-    showSearchWithGoogle: false
+    showSearchWithGoogle: false,
+    prepend: (defaultActions, parameters) => [
+      {
+        label: 'Search Google for “{selection}”',
+        visible: parameters.selectionText.trim().length > 0,
+        click: () => {
+          const mainWindow = getMainWindow()
+          mainWindow?.webContents.send(
+            'open-url',
+            `https://google.com/search?q=${encodeURIComponent(parameters.selectionText)}`
+          )
+        }
+      },
+      {
+        label: 'Search Perplexity for “{selection}”',
+        visible: parameters.selectionText.trim().length > 0,
+        click: () => {
+          const mainWindow = getMainWindow()
+          mainWindow?.webContents.send(
+            'open-url',
+            `https://www.perplexity.ai/?q=${encodeURIComponent(parameters.selectionText)}`
+          )
+        }
+      },
+      defaultActions.separator(),
+      {
+        label: 'Open in New Card',
+        visible: parameters.linkURL.length > 0 && parameters.mediaType === 'none',
+        click: () => {
+          const mainWindow = getMainWindow()
+          mainWindow?.webContents.send('open-url', parameters.linkURL)
+        }
+      }
+    ]
   }
 
   const opts = Object.assign(defaultOpts, options)
