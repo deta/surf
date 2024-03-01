8d83949626eefc56b9e1645f09a82375d74d904f
Maximilian <github@maximilian-heidenreich.de>
2024-03-01 19:28:12 +0100

    am fixes (#208)
    * fix: prevent click from creating a card without dragging at least one unit
    
    * fix enter to select on visor & improve closing animation
    
    * fix Visor freezing horizon after delete all cards & make Escape close Visor
    
    * quick switch cards with option + tab
    
    * respect media imports image aspect ratio
    
    * improve media import aspect ratio calulations
    
    * fix auto focus closest to center if opt+tab
    
    * fix: browser bar still active in visor

--- packages/core/src/lib/components/Horizon/MediaImporter.svelte
+++ packages/core/src/lib/components/Horizon/MediaImporter.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { hasClassOrParentWithClass, posToAbsolute } from '@horizon/tela'
+  import { clamp, hasClassOrParentWithClass, posToAbsolute } from '@horizon/tela'
   import type { Horizon } from '../../service/horizon'
   import { useLogScope } from '../../utils/log'
   import { parseClipboardItems } from '../../service/clipboard'
@@ -244,11 +244,25 @@
   }
 
   const createImageCard = async (blob: Blob, pos: { x: number; y: number }) => {
+    // Find out size
+    const src = URL.createObjectURL(blob)
+    const [imgWidth, imgHeight] = await new Promise((resolve, reject) => {
+      let img = new Image()
+      img.onload = () => resolve([img.width, img.height])
+      img.onerror = reject
+      img.src = src
+    })
+
+    // TODO: Make this respect our default card size a bit better
+    const aspect = clamp(imgWidth / imgHeight, 0, 4)
+    let targetWidth = DEFAULT_CARD_SIZE.width * aspect
+    let targetHeight = DEFAULT_CARD_SIZE.width
+
     const card = await horizon.addCardFile(blob, {
       x: pos.x - DEFAULT_CARD_SIZE.width / 2,
       y: pos.y - DEFAULT_CARD_SIZE.height / 2,
-      width: DEFAULT_CARD_SIZE.width,
-      height: DEFAULT_CARD_SIZE.height
+      width: targetWidth,
+      height: targetHeight
     })
     log.debug('created card', get(card))
   }
--- packages/core/src/lib/constants/card.ts
+++ packages/core/src/lib/constants/card.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_CARD_SIZE = {
-  width: 300,
-  height: 200
+  width: 30 * 11,
+  height: 30 * 7
 }
