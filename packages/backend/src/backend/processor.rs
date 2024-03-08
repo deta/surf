604699b2c30c337bb5dfb3e7947ba7c2f9088ae2
brkp <brkp@tutanota.com>
2024-03-08 10:50:08 +0300

    packages: backend: post-process collectables
    Signed-off-by: brkp <brkp@tutanota.com>

--- packages/backend/src/backend/processor.rs
+++ packages/backend/src/backend/processor.rs
@@ -1,40 +1,30 @@
 use super::{message::*, tunnel::WorkerTunnel};
-use crate::store::db::CompositeResource;
+use crate::{store::db::CompositeResource, BackendResult};
+
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
 pub fn processor_thread_entry_point(tunnel: WorkerTunnel) {
     while let Ok(message) = tunnel.tqueue_rx.recv() {
         match message {
             ProcessorMessage::ProcessResource(resource) => {
-                handle_process_resource(&tunnel, resource)
+                let _ = handle_process_resource(&tunnel, resource)
+                    .map_err(|e| eprintln!("error while processing resource: {e:?}"));
             }
         }
     }
 }
 
-fn handle_process_resource(tunnel: &WorkerTunnel, resource: CompositeResource) {
+fn handle_process_resource(
+    tunnel: &WorkerTunnel,
+    resource: CompositeResource,
+) -> BackendResult<()> {
+    if !needs_processing(&resource.resource.resource_type) {
+        return Ok(());
+    }
+
     let resource_data = std::fs::read_to_string(&resource.resource.resource_path).unwrap();
-
-    let output = match resource.resource.resource_type.as_str() {
-        "application/vnd.space.document.space-note" => {
-            let mut output = String::new();
-            let mut in_tag = false;
-
-            for c in resource_data.chars() {
-                match (in_tag, c) {
-                    (true, '>') => in_tag = false,
-                    (false, '<') => {
-                        in_tag = true;
-                        output.push(' ');
-                    }
-                    (false, _) => output.push(c),
-                    _ => (),
-                }
-            }
-
-            Some(output)
-        }
-        _ => None,
-    };
+    let output = process_resource_data(&resource, &resource_data);
 
     if let Some(output) = output {
         let output = output
@@ -53,4 +43,247 @@ fn handle_process_resource(tunnel: &WorkerTunnel, resource: CompositeResource) {
             None,
         );
     }
+
+    Ok(())
+}
+
+fn needs_processing(resource_type: &str) -> bool {
+    match resource_type {
+        _ if resource_type.starts_with("application/vnd.space.") => true,
+        _ => false,
+    }
+}
+
+fn process_resource_data(resource: &CompositeResource, resource_data: &str) -> Option<String> {
+    match resource.resource.resource_type.as_str() {
+        // TODO: mb we should have this use the same format as the post resources?
+        "application/vnd.space.document.space-note" => Some(normalize_html_data(resource_data)),
+
+        resource_type if resource_type.starts_with("application/vnd.space.post") => {
+            serde_json::from_str::<PostData>(resource_data)
+                .map_err(|e| eprintln!("deserializing post data: {e:#?}"))
+                .ok()
+                .map(|post_data| {
+                    format!(
+                        "{} {} {} {} {}",
+                        post_data.title.unwrap_or_default(),
+                        post_data.excerpt.unwrap_or_default(),
+                        post_data.content_plain,
+                        post_data.author.unwrap_or_default(),
+                        post_data.site_name
+                    )
+                })
+        }
+        resource_type if resource_type.starts_with("application/vnd.space.chat-message") => {
+            serde_json::from_str::<ChatMessageData>(resource_data)
+                .map_err(|e| eprintln!("deserializing chat message data: {e:#?}"))
+                .ok()
+                .map(|message_data| {
+                    format!(
+                        "{} {} {}",
+                        message_data.author, message_data.content_plain, message_data.platform_name
+                    )
+                })
+        }
+        resource_type if resource_type.starts_with("application/vnd.space.document") => {
+            serde_json::from_str::<DocumentData>(resource_data)
+                .map_err(|e| eprintln!("deserializing document data: {e:#?}"))
+                .ok()
+                .map(|document_data| {
+                    format!(
+                        "{} {} {}",
+                        document_data.author.unwrap_or_default(),
+                        document_data.content_plain,
+                        document_data.editor_name
+                    )
+                })
+        }
+        resource_type if resource_type.starts_with("application/vnd.space.article") => {
+            serde_json::from_str::<ArticleData>(resource_data)
+                .map_err(|e| eprintln!("deserializing article data: {e:#?}"))
+                .ok()
+                .map(|article_data| {
+                    format!(
+                        "{} {} {} {} {}",
+                        article_data.title,
+                        article_data.excerpt.unwrap_or_default(),
+                        article_data.content_plain,
+                        article_data.author.unwrap_or_default(),
+                        article_data.site_name
+                    )
+                })
+        }
+        resource_type if resource_type.starts_with("application/vnd.space.link") => {
+            serde_json::from_str::<LinkData>(resource_data)
+                .map_err(|e| eprintln!("deserializing link data: {e:#?}"))
+                .ok()
+                .map(|link_data| {
+                    format!(
+                        "{} {} {}",
+                        link_data.title,
+                        link_data.description.unwrap_or_default(),
+                        link_data.url
+                    )
+                })
+        }
+        resource_type if resource_type.starts_with("application/vnd.space.chat-thread") => {
+            serde_json::from_str::<ChatThreadData>(resource_data)
+                .map_err(|e| eprintln!("deserializing chat thread data: {e:#?}"))
+                .ok()
+                .map(|thread_data| {
+                    let messages_content = thread_data
+                        .messages
+                        .iter()
+                        .map(|msg| msg.content_plain.clone())
+                        .collect::<Vec<_>>()
+                        .join(" ");
+                    format!(
+                        "{} {}",
+                        thread_data.title.unwrap_or_default(),
+                        messages_content
+                    )
+                })
+        }
+        _ => None,
+    }
+}
+
+fn normalize_html_data(data: &str) -> String {
+    let mut output = String::new();
+    let mut in_tag = false;
+
+    for c in data.chars() {
+        match (in_tag, c) {
+            (true, '>') => in_tag = false,
+            (false, '<') => {
+                in_tag = true;
+                output.push(' ');
+            }
+            (false, _) => output.push(c),
+            _ => (),
+        }
+    }
+
+    output
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct PostData {
+    author: Option<String>,
+    author_fullname: Option<String>,
+    author_image: Option<String>,
+    author_url: Option<String>,
+    content_html: String,
+    content_plain: String,
+    date_edited: Option<String>,
+    date_published: String,
+    edited: Option<bool>,
+    excerpt: Option<String>,
+    images: Vec<String>,
+    lang: Option<String>,
+    links: Vec<String>,
+    parent_title: Option<String>,
+    parent_url: Option<String>,
+    post_id: String,
+    site_icon: String,
+    site_name: String,
+    stats: PostStats,
+    title: Option<String>,
+    url: String,
+    video: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct PostStats {
+    comments: Option<i32>,
+    down_votes: Option<i32>,
+    up_votes: Option<i32>,
+    views: Option<i32>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ChatMessageData {
+    author: String,
+    author_image: Option<String>,
+    author_url: Option<String>,
+    content_html: String,
+    content_plain: String,
+    date_edited: Option<String>,
+    date_sent: String,
+    images: Vec<String>,
+    in_reply_to: Option<String>,
+    #[serde(rename = "messageId")]
+    message_id: String,
+    parent_title: Option<String>,
+    parent_url: Option<String>,
+    platform_icon: String,
+    platform_name: String,
+    url: String,
+    video: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct DocumentData {
+    author: Option<String>,
+    author_fullname: Option<String>,
+    author_image: Option<String>,
+    author_url: Option<String>,
+    content_html: String,
+    content_plain: String,
+    date_created: String,
+    date_edited: Option<String>,
+    editor_icon: String,
+    editor_name: String,
+    url: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ArticleData {
+    author: Option<String>,
+    author_image: Option<String>,
+    author_url: Option<String>,
+    category_name: Option<String>,
+    category_url: Option<String>,
+    content_html: String,
+    content_plain: String,
+    date_published: Option<String>,
+    date_updated: Option<String>,
+    direction: Option<String>,
+    excerpt: Option<String>,
+    images: Vec<String>,
+    lang: Option<String>,
+    site_icon: String,
+    site_name: String,
+    stats: HashMap<String, Option<i32>>,
+    title: String,
+    url: String,
+    word_count: i32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct LinkData {
+    author: Option<String>,
+    date_modified: Option<String>,
+    date_published: Option<String>,
+    description: Option<String>,
+    icon: Option<String>,
+    image: Option<String>,
+    keywords: Vec<String>,
+    language: Option<String>,
+    provider: Option<String>,
+    title: String,
+    url: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ChatThreadData {
+    content_plain: String,
+    creator: String,
+    creator_image: Option<String>,
+    creator_url: Option<String>,
+    messages: Vec<ChatMessageData>,
+    platform_icon: String,
+    platform_name: String,
+    title: Option<String>,
+    url: String,
 }
