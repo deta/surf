3943c11e62cd02c2f6f2abfc2434b4eea025b7da
Cristian Cretu <crisemcr@gmail.com>
2024-07-14 20:32:17 +0200

    feat: add shadcn

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -2,10 +2,16 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { plugin as Markdown } from 'vite-plugin-markdown'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [Markdown(), svelte()],
+  resolve: {
+    alias: {
+      $lib: path.resolve('./src/lib')
+    }
+  },
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
