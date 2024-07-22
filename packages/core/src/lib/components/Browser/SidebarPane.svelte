976d95296f7f114bc97723c0253aac0f245a1242
Cristian Cretu <crisemcr@gmail.com>
2024-07-22 17:20:57 +0200

    save

--- packages/core/src/lib/components/Browser/SidebarPane.svelte
+++ packages/core/src/lib/components/Browser/SidebarPane.svelte
@@ -23,106 +23,37 @@
   }
 </script>
 
-{#if horizontalTabs}
-  <!-- <PaneGroup direction={'vertical'} autoSaveId="surf-sidebar-horizontal" class="relative flex flex-col">
+<PaneGroup direction={horizontalTabs ? 'vertical' : 'horizontal'}>
+  {#if horizontalTabs}
     <Pane
-      minSize={2}
-      maxSize={6}
       defaultSize={3}
-      onCollapse={handleCollapse}
-      onExpand={handleExpand}
-    >
-      <slot name="sidebar" />
-    </Pane>
-    <PaneResizer class="hover:bg-neutral-100 z-[50001]">
-      <div
-        class:h-full={!horizontalTabs}
-        class:w-0.5={!horizontalTabs}
-        class:w-full={horizontalTabs}
-        class:h-0.5={horizontalTabs}
-      />
-    </PaneResizer>
-    <PaneGroup direction="horizontal">
-      <Pane maxSize={10}>
-        <slot name="content" />
-      </Pane>
-    </PaneGroup>
-  </PaneGroup> -->
-
-  <PaneGroup direction="vertical" autoSaveId="surf-sidebar-horizontal">
-	<Pane
-		defaultSize={10}
-		collapsedSize={2}
-		collapsible={true}
-		minSize={2}
-        maxSize={20}
-        class="bg-white"
-	>
-        <slot name="sidebar" />
-    </Pane>
-    <PaneResizer class="hover:bg-neutral-100 z-[50001]">
-        <div
-          class:h-full={!horizontalTabs}
-          class:w-0.5={!horizontalTabs}
-          class:w-full={horizontalTabs}
-          class:h-0.5={horizontalTabs}
-        />
-      </PaneResizer>
-	<Pane maxSize={100}>
-		<PaneGroup direction="horizontal">
-			<Pane defaultSize={50}>
-                <slot name="content" />
-            </Pane>
-            <PaneResizer class="hover:bg-neutral-100 z-[50001]">
-                <div
-                  class:h-full={!horizontalTabs}
-                  class:w-0.5={!horizontalTabs}
-                  class:w-full={horizontalTabs}
-                  class:h-0.5={horizontalTabs}
-                />
-              </PaneResizer>
-			<Pane defaultSize={20} />
-		</PaneGroup>
-	</Pane>
-</PaneGroup>
-
-{:else}
-  <!-- <PaneGroup direction={'horizontal'} autoSaveId="surf-sidebar-vertical" class="relative flex flex-row">
-    <Pane
+      collapsedSize={2}
+      collapsible={true}
       minSize={2}
       maxSize={20}
-      defaultSize={10}
-      onCollapse={handleCollapse}
-      onExpand={handleExpand}
     >
       <slot name="sidebar" />
     </Pane>
-    <PaneResizer class="hover:bg-neutral-100 z-[50001]">
-      <div
-        class:h-full={!horizontalTabs}
-        class:w-0.5={!horizontalTabs}
-        class:w-full={horizontalTabs}
-        class:h-0.5={horizontalTabs}
-      />
-    </PaneResizer>
+  {:else}
+    <Pane defaultSize={10} collapsedSize={2} collapsible={true} minSize={2} maxSize={20}>
+      <slot name="sidebar" />
+    </Pane>
+  {/if}
+
+  <PaneResizer class="hover:bg-neutral-100 z-[50001]">
+    <div
+      class:h-full={!horizontalTabs}
+      class:w-0.5={!horizontalTabs}
+      class:w-full={horizontalTabs}
+      class:h-0.5={horizontalTabs}
+    />
+  </PaneResizer>
+  <Pane maxSize={100}>
     <PaneGroup direction="horizontal">
-      <Pane maxSize={50}>
+      <Pane defaultSize={50}>
         <slot name="content" />
       </Pane>
-    </PaneGroup>
-  </PaneGroup> -->
-
-  <PaneGroup direction="horizontal" autoSaveId="surf-sidebar-vertical">
-	<Pane
-		defaultSize={10}
-		collapsedSize={2}
-		collapsible={true}
-		minSize={2}
-        maxSize={20}
-	>
-        <slot name="sidebar" />
-    </Pane>
-    <PaneResizer class="hover:bg-neutral-100 z-[50001]">
+      <PaneResizer class="hover:bg-neutral-100 z-[50001]">
         <div
           class:h-full={!horizontalTabs}
           class:w-0.5={!horizontalTabs}
@@ -130,23 +61,7 @@
           class:h-0.5={horizontalTabs}
         />
       </PaneResizer>
-	<Pane maxSize={100}>
-		<PaneGroup direction="horizontal">
-			<Pane defaultSize={50}>
-                <slot name="content" />
-            </Pane>
-            <PaneResizer class="hover:bg-neutral-100 z-[50001]">
-                <div
-                  class:h-full={!horizontalTabs}
-                  class:w-0.5={!horizontalTabs}
-                  class:w-full={horizontalTabs}
-                  class:h-0.5={horizontalTabs}
-                />
-              </PaneResizer>
-			<Pane defaultSize={20} />
-		</PaneGroup>
-	</Pane>
+      <Pane defaultSize={20} />
+    </PaneGroup>
+  </Pane>
 </PaneGroup>
-
-{/if}
-
