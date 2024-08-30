df05d3387c4a50dbbfdad4d93fafb72eee597bd5
brkp <brkp@tutanota.com>
2024-08-31 00:40:07 +0300

    packages: trackpad: fix build issues for non-macos
    Signed-off-by: brkp <brkp@tutanota.com>

--- packages/trackpad/src/trackpad.rs
+++ packages/trackpad/src/trackpad.rs
@@ -3,9 +3,9 @@ pub use crate::macos::*;
 
 #[cfg(not(target_os = "macos"))]
 mod other {
-    pub fn set_scroll_began_callback<F: Fn() + Send + Sync + 'static>(_callback: F) {}
-    pub fn set_scroll_ended_callback<F: Fn() + Send + Sync + 'static>(_callback: F) {}
-    pub fn set_force_click_callback<F: Fn() + Send + Sync + 'static>(callback: F) {}
+    pub fn set_scroll_start_callback<F: Fn() + Send + Sync + 'static>(_callback: F) {}
+    pub fn set_scroll_stop_callback<F: Fn() + Send + Sync + 'static>(_callback: F) {}
+    pub fn set_force_click_callback<F: Fn() + Send + Sync + 'static>(_callback: F) {}
 
     pub fn initialize_trackpad_monitor() {}
 }
