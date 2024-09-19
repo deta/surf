50f63037f2100b0d09439cfdcac3902941977156
Aavash Shrestha <aavshr@gmail.com>
2024-09-19 16:36:08 +0200

    integrate sentry (#732)
    * integrate sentry
    
    * init sentry in settings, setup renderer pages
    
    * add sentry vite plugin only as dev dependency

--- apps/desktop/src/main/env.d.ts
+++ apps/desktop/src/main/env.d.ts
@@ -8,6 +8,7 @@ interface ImportMetaEnv {
   readonly M_VITE_USE_TMP_DATA_DIR?: string
   readonly M_VITE_DISABLE_AUTO_UPDATE?: string
   readonly M_VITE_CREATE_SETUP_WINDOW?: string
+  readonly M_VITE_SENTRY_DSN?: string
 }
 
 interface ImportMeta {
