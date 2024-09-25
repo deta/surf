289c070f328e5b128a0e0452354fddc13a6c7e64
Felix Tesche <felix.tesche@gmail.com>
2024-09-25 12:35:07 +0200

    fixed a bug where .tempspace was visible in the context menu

--- packages/core/src/lib/components/Resources/ResourcePreview.svelte
+++ packages/core/src/lib/components/Resources/ResourcePreview.svelte
@@ -570,7 +570,11 @@
         items: $spaces
           ? [
               ...$spaces
-                .filter((e) => e.name.folderName.toLowerCase() !== 'all my stuff')
+                .filter(
+                  (e) =>
+                    e.name.folderName.toLowerCase() !== 'all my stuff' &&
+                    e.name.folderName.toLowerCase() !== '.tempspace'
+                )
                 .sort((a, b) => new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime())
                 .map((space) => ({
                   type: 'action',
